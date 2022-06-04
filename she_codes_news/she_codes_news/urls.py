67f90589489f8094d284402be62876c9b9bf603d
RicStar2022 <ricci.lee@bigpond.com>
2022-06-04 12:57:11 +1000

    Saturday class
    saturday class

--- she_codes_news/she_codes_news/urls.py
+++ she_codes_news/she_codes_news/urls.py
@@ -20,5 +20,6 @@ include, path
 urlpatterns = [
     path('news/', include('news.urls')),
     path('admin/', admin.site.urls),
-
+    path('users/', include('users.urls')),
+    path('users/', include('django.contrib.auth.urls')),
 ]
